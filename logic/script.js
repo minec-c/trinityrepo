61bb5fe03eea1cbf3f7d903fb59fe072036f0014
minec-c <reciprocalidea@gmail.com>
2024-12-25 23:15:27 -0300

    Create script.js
    logic of script.js in logic folder

---
+++ logic/script.js
@@ -0,0 +1,12 @@
+  const imageContainer = document.querySelector('.Charactersp');
+  const sprite = document.querySelector('.Mainspsheet');
+  const Charactersp = document.getElementById("Charactersp");
+  let posX = 0, posY = 0; 				// Current position
+  const speed = 2;
+  let animationFrameId; 				// To control the animation frame
+  const treesp = document.querySelector('.Greentreespdiv');
+  var div1 = imageContainer.getBoundingClientRect();
+  var div2 = treesp.getBoundingClientRect();
+
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
