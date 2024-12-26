9d426d03bcdab779e489050f93e0444488c83520
minec-c <reciprocalidea@gmail.com>
2024-12-25 23:20:53 -0300

    Create script5.js
    Logic of script5.js in logic folder

---
+++ logic/script5.js
@@ -0,0 +1,5 @@
+  document.addEventListener('click', (event) => {
+    const targetX = event.clientX - imageContainer.offsetWidth / 2.2;//1.4
+    const targetY = event.clientY - imageContainer.offsetHeight / 1.9;//1.3
+    moveToTarget(targetX, targetY);
+  });
