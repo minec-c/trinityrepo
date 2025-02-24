3222b375e2c4d6baae12bed495b51515f19b5c7b
minec-c <reciprocalidea@gmail.com>
2025-02-23 22:40:13 -0300

    Update script5.js

--- logic/script5.js
+++ logic/script5.js
@@ -2,4 +2,5 @@
     const targetX = event.clientX - imageContainer.offsetWidth / 2.2;//1.4
     const targetY = event.clientY - imageContainer.offsetHeight / 1.9;//1.3
     moveToTarget(targetX, targetY);
+    
   });
